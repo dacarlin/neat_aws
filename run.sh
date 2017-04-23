8cbc3c83168453d16e095c0dddf7726982c95552
Alex Carlin <dacarlin@users.noreply.github.com>
2017-04-22 20:24:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
-subread=input_files/*subreadset.xml
-barode=barcodes.fasta
-/home/ec2-user/smrtlink/smrtcmds/bin/pbsmrtpipe -e eid_subread:$subread -e eid_barcode:$barcode --preset-xml preset.xml 
+
+subread=`ls /home/ec2-user/input_files/*subreadset.xml`
+barcode=/home/ec2-user/neat_aws/barcodes.fasta
+preset=/home/ec2-user/neat_aws/preset.xml 
+
+/home/ec2-user/smrtlink/smrtcmds/bin/pbsmrtpipe -e eid_subread:$subread -e eid_barcode:$barcode --preset-xml $preset
 
