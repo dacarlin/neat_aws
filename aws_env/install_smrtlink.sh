48ee9cf784c56f72ef025fde67f98a89d8f87179
Alex Carlin <carlin@ucdavis.edu>
2017-05-05 14:43:37 -0700

    Updated AWS environment

--- aws_env/install_smrtlink.sh
+++ aws_env/install_smrtlink.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 curl -O https://downloads.pacbcloud.com/public/software/installers/smrtlink_4.0.0.190159.zip
 unzip -P SmrT3chN smrtlink_4.0.0.190159.zip
 ./smrtlink_4.0.0.190159.run --rootdir smrt
--- aws_env/long_amplicon_analysis.sh
+++ aws_env/long_amplicon_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pbsmrtpipe pipeline-id pbsmrtpipe.pipelines.sa3_ds_barcode_laa \
-  -e eid_subread:input_files/m54048_160616_015450.subreadset.xml \
-  -e eid_barcode:neat.barcodeset.xml \
+  -e eid_subread:input_files/*.subreadset.xml \
+  -e eid_barcode:bc.barcodeset.xml \
   --preset-xml laa.xml \
   --preset-xml global.xml \
-  -o job__laa_nophase_nocluster
+  -o job__laa_phase_cluster_off
